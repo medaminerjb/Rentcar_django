2ca82b74efba293530ed77084d88e7a523294b6c
medaminerjb <meda30449@gmail.com>
2024-04-08 04:03:24 +0100

    fixes

--- rentcar/dashboardapp/models.py
+++ rentcar/dashboardapp/models.py
@@ -6,26 +6,26 @@ import uuid
 class ModuleVoiture(models.Model):
     id_module = models.AutoField(primary_key=True)
     name_module = models.CharField(max_length=255,null=False)
-    marque = models.CharField(max_length=63,null=False)
-    puissance = models.IntegerField(max_length=2,null=True)
-    nbr_place = models.IntegerField(max_length=2,null=True)
-    reservoir = models.IntegerField(max_length=7,null=True)
-    prix_neuf = models.IntegerField(max_length=11,null=True)
-    etat = models.CharField(max_length=5,null=False,default='neuf')
+    marque = models.CharField(max_length=63,null=True)
+    puissance = models.IntegerField(null=True)
+    nbr_place = models.IntegerField(null=True)
+    reservoir = models.IntegerField(null=True)
+    prix_neuf = models.IntegerField(null=True)
+    etat = models.CharField(max_length=5,null=True,default='neuf')
     desponiblte = models.CharField(max_length=55,null=True)
-    created = models.DateTimeField(auto_now_add=True, editable=False)
-    last_updated = models.DateTimeField(auto_now=True, editable=False)
+    created = models.DateTimeField(auto_now_add=True,null=True, editable=False)
+    last_updated = models.DateTimeField(auto_now=True,null=True, editable=False)
 
     def __str__(self) -> str:
         return str(self.name_module)
-class voiture(models.Model):
+class Voiture(models.Model):
     id = models.AutoField(primary_key=True)
     numeroMat = models.CharField(max_length=127, unique=True)
-    modulecar = models.CharField(max_length=127)
-    couleur = models.CharField(max_length=63)
-    type = models.CharField(max_length=63)
-    modulevoiture = models.ForeignKey(ModuleVoiture,blank=True,on_delete=models.CASCADE)
-    type = models.CharField(max_length=50)
+    modulecar = models.CharField(max_length=127,null=True)
+    couleur = models.CharField(max_length=63,null=True)
+    type = models.CharField(max_length=63,null=True)
+    modulevoiture = models.ForeignKey(ModuleVoiture,blank=True,null=True,on_delete=models.CASCADE)
+    type = models.CharField(max_length=50,null=True)
 
     def __str__(self):
         return str(self.numeroMat)
@@ -33,7 +33,7 @@ class Client(models.Model):
     id_client = models.AutoField(primary_key=True)
     name = models.CharField(max_length=127,null=False)
     email = models.EmailField(null=True)
-    numerotel = models.IntegerField(max_length=8,null=True)
+    numerotel = models.IntegerField(null=True)
     user= models.ForeignKey(User,blank=True,on_delete=models.CASCADE)
 
     def __str__(self):
@@ -42,21 +42,22 @@ class agent(models.Model):
     id_agent = models.AutoField(primary_key=True)
     name = models.CharField(max_length=127,null=False)
     email = models.EmailField(null=True)
-    numerotel = models.IntegerField(max_length=8,null=True)
-    user= models.ForeignKey(User)
+    numerotel = models.IntegerField(null=True)
+    user= models.ForeignKey(User,blank=True,on_delete=models.CASCADE)
     def __str__(self):
         return str(self.name)
 
 class reservation(models.Model):
     id_reservation = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    place = models.CharField(blank=True,null=True)
-    status = models.CharField(blank=True,null=True)
+    place = models.CharField(max_length=255,blank=True,null=True)
+    status = models.CharField(max_length=255,blank=True,null=True)
     date_debut = models.DateField(blank=True, null=True)
+    
     date_fin = models.DateField(blank=True, null=True)
     date_reservation = models.DateField(blank=True, null=True)
     client = models.ForeignKey(Client,blank=True,on_delete=models.CASCADE)
-    voiture = models.ForeignKey(voiture,blank=True,on_delete=models.CASCADE)
-    numero_tel = models.IntegerField(max_length=8,null=False)
+    voiture = models.ForeignKey(Voiture,blank=True,on_delete=models.CASCADE)
+    numero_tel = models.IntegerField(null=False)
 
     created = models.DateTimeField(auto_now_add=True, editable=False)
     last_updated = models.DateTimeField(auto_now=True, editable=False)
@@ -65,14 +66,14 @@ class reservation(models.Model):
         return str(self.id_reservation)
 class sous_traitance(models.Model):
     id_sous_traitance = models.AutoField(primary_key=True)
-    taux_echeance = models.IntegerField(max_length=11,blank=True,null=True)
-    number_echeance = models.IntegerField(max_length=4,null=True)
-    number_echance_paier = models.IntegerField(max_length=4,null=True)
-    avance = models.IntegerField(max_length=11,null=True)
+    taux_echeance = models.IntegerField(blank=True,null=True)
+    number_echeance = models.IntegerField(null=True)
+    number_echance_paier = models.IntegerField(null=True)
+    avance = models.IntegerField(null=True)
     date_paiment = models.DateField(blank=True,null=True)
     date_debut = models.DateField(blank=True,null=True)
     date_fin = models.DateField(blank=True,null=True)
-    voiture = models.ForeignKey(voiture,blank=True,on_delete=models.CASCADE)
+    voiture = models.ForeignKey(Voiture,blank=True,on_delete=models.CASCADE)
     agent = models.ForeignKey(agent,blank=True,on_delete=models.CASCADE)
 
     def __str__(self) -> str:
@@ -80,11 +81,11 @@ class sous_traitance(models.Model):
 
 class historique_st(models.Model):
     id_echeance = models.AutoField(primary_key=True)
-    echeance = models.IntegerField(max_length=11,null=True)
+    echeance = models.IntegerField(null=True)
     date_paiment = models.DateField(blank=True,null=True)
-    staut = models.CharField(blank=True,null=True)
+    staut = models.CharField(max_length=55,blank=True,null=True)
     sous_traitance = models.ForeignKey(sous_traitance,blank=True,on_delete=models.CASCADE)
-    voiture = models.ForeignKey(voiture,blank=True,on_delete=models.CASCADE)
+    voiture = models.ForeignKey(Voiture,blank=True,on_delete=models.CASCADE)
 
     created = models.DateTimeField(auto_now_add=True, editable=False)
     last_updated = models.DateTimeField(auto_now=True, editable=False)
