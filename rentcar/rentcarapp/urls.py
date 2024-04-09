bb46b66118c25c3c6c110d8ea66f2c3160c5a9dd
medaminerjb <meda30449@gmail.com>
2024-04-09 05:06:00 +0100

    add view signin , sign up and listing view cars

--- rentcar/rentcarapp/urls.py
+++ rentcar/rentcarapp/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('home', views.home, name='home'),
     path('voitures_listing',views.voitures_listing,name='voituresview'),
     path('login/', views.loginView, name='login'),
+    path('signup/',views.signupView,name='signup'),
     path('logout/', views.LogoutView, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
 ]
